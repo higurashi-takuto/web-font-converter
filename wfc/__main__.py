2bad25df39f060e0d207da7597b0d73ab57f6e78
higurashi-takuto <contact@hgrs.me>
2020-03-31 22:54:59 +0900

    コメントの追加

--- wfc/__main__.py
+++ wfc/__main__.py
@@ -4,17 +4,28 @@ from fontTools.ttLib import TTFont
 
 
 def convert_font(input_path, output_path, flavor):
+    '''
+    フォントの形式を選択して出力する。
+    '''
     font = TTFont(input_path)
     font.flavor = flavor
     font.save(output_path)
 
 
 def wfc():
+    '''
+    入力がフォントの場合は WOFF / WOFF2 を出力する。
+    入力が Web フォントの場合は内容に応じて OTF / TTF を出力する。
+    それぞれ出力は拡張子のみを変えたパスに行う。
+    '''
+    # 引数処理
     parser = argparse.ArgumentParser(description='フォント ⇄ Web フォント')
     parser.add_argument('input', help='入力フォントへのパス')
     args = parser.parse_args()
 
     input_path, ext = os.path.splitext(args.input)
+
+    # 入力がフォントの場合
     if ext in ['.ttf', '.otf']:
         output_path = f'{input_path}.woff'
         convert_font(args.input, output_path, 'woff')
@@ -22,6 +33,7 @@ def wfc():
         output_path = f'{input_path}.woff2'
         convert_font(args.input, output_path, 'woff2')
 
+    # 入力が Web フォントの場合
     elif ext in ['.woff', '.woff2']:
         font = TTFont(args.input)
         if ('CFF ' in font) or ('CFF2' in font):
@@ -33,12 +45,17 @@ def wfc():
 
 
 def wfc_woff():
+    '''
+    入力フォントを WOFF に変換し出力する。
+    '''
+    # 引数処理
     parser = argparse.ArgumentParser(description='WOFF へ変換')
     parser.add_argument('input', help='入力フォントへのパス')
     parser.add_argument('--output', '-o', default='',
                         help='入力フォントへのパス（ デフォルトは拡張子のみ変更 ）')
     args = parser.parse_args()
 
+    # Web フォントの出力
     if args.output:
         output_path = args.output
     else:
@@ -49,12 +66,17 @@ def wfc_woff():
 
 
 def wfc_woff2():
+    '''
+    入力フォントを WOFF2 に変換し出力する。
+    '''
+    # 引数処理
     parser = argparse.ArgumentParser(description='WOFF2 へ変換')
     parser.add_argument('input', help='入力フォントへのパス')
     parser.add_argument('--output', '-o', default='',
                         help='入力フォントへのパス（ デフォルトは拡張子のみ変更 ）')
     args = parser.parse_args()
 
+    # Web フォントの出力
     if args.output:
         output_path = args.output
     else:
@@ -65,12 +87,18 @@ def wfc_woff2():
 
 
 def wfc_otf():
+    '''
+    入力フォントを OTF に変換し出力する。
+    現在、中身は TTF と同じ処理で拡張子のみが異なる。
+    '''
+    # 引数処理
     parser = argparse.ArgumentParser(description='OTF へ変換')
     parser.add_argument('input', help='入力フォントへのパス')
     parser.add_argument('--output', '-o', default='',
                         help='入力フォントへのパス（ デフォルトは拡張子のみ変更 ）')
     args = parser.parse_args()
 
+    # フォントの出力
     if args.output:
         output_path = args.output
     else:
@@ -81,12 +109,18 @@ def wfc_otf():
 
 
 def wfc_ttf():
+    '''
+    入力フォントを TTF に変換し出力する。
+    現在、中身は OTF と同じ処理で拡張子のみが異なる。
+    '''
+    # 引数処理
     parser = argparse.ArgumentParser(description='TTF へ変換')
     parser.add_argument('input', help='入力フォントへのパス')
     parser.add_argument('--output', '-o', default='',
                         help='入力フォントへのパス（ デフォルトは拡張子のみ変更 ）')
     args = parser.parse_args()
 
+    # フォントの出力
     if args.output:
         output_path = args.output
     else:
